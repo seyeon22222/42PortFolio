40f8c16681c518224724c59f8efaf0063389e2f9
seyeon22222 <92151066+seyeon22222@users.noreply.github.com>
2024-01-12 11:51:28 +0000

    Change : CPP08

--- Circle5/CPP08/ex01/Span.hpp
+++ Circle5/CPP08/ex01/Span.hpp
@@ -18,6 +18,17 @@ public:
 	void addNumber(int);
 	int shortestSpan();
 	int longestSpan();
+	template<class T>
+	void addNumbers(typename T::iterator s, typename T::iterator e)
+	{
+		if (e <= s)
+			throw std::exception();
+		size_t sub = e - s;
+		for (size_t i = 0; i < sub; i++)
+			this->addNumber(*(s + i));
+	}
 };
+
+
 
 #endif
--- Circle5/CPP08/ex01/main.cpp
+++ Circle5/CPP08/ex01/main.cpp
@@ -1,11 +1,13 @@
 #include "Span.hpp"
 #include <iostream>
-
+#include <vector>
 int main()
 {
-	Span sp = Span(10000);
+	std::vector<int> a;
 	for (int i = 0; i < 10000; i++)
-		sp.addNumber(i);
+		a.push_back(i);
+	Span sp = Span(10000);
+	sp.addNumbers<std::vector<int> >(a.begin(), a.end());
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 	return 0;
