00dbca52e6e44a301ca926787accecebd54754fb
seyeon22222 <kksy0917@gmail.com>
2024-01-30 14:30:16 +0900

    ADD : Circle5 subjects

---
+++ Circle5/Inception/srcs/requirements/tools/fclean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+wordpress_path=$($(pwd)/srcs/requirements/tools/info.sh wordpress_path)
+mariadb_path=$($(pwd)/srcs/requirements/tools/info.sh mariadb_path)
+conf=$($(pwd)/srcs/requirements/tools/info.sh conf)
+hosts_path=$($(pwd)/srcs/requirements/tools/info.sh hosts_path)
+
+rm -rf ${wordpress_path}
+rm -rf ${mariadb_path}
+
+# 리눅스와 mac의 sed 사용법이 다르다...
+# * ----------------------------------------------------------
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    sed -i "/${conf}/d" ${hosts_path}
+fi
+# * ----------------------------------------------------------
+
+echo "\
+--------------------
+
+@fclean done
+
+--------------------"
