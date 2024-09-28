494aec26135937f2bb1c7c91e6d2c90e09cfcbc5
seykim <kksy0917@gmail.com>
2024-09-28 20:30:42 +0900

    circle6 fix

--- Circle6/ft_transcendence_v2/BackEnd/User/view/User_login.py
+++ Circle6/ft_transcendence_v2/BackEnd/User/view/User_login.py
@@ -1,32 +1,65 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
+from rest_framework import status, serializers
 from ..serializer import UserSerializer
 from ..model import MyUser
 from ..utils import validate_input, validate_password
 from django.contrib.auth import authenticate, login
 
+class UserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+# class User_login(APIView):
+
+#     queryset = MyUser.MyUser.objects.all()
+#     serializer_class = UserSerializer.UserSerializer
+    
+#     def post(self, request):
+#         username = request.data.get('username')
+#         password = request.data.get('password')
+
+#         check, message = validate_input(username)
+#         if not check:
+#             return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        
+#         check, message = validate_password(username, password)
+#         if not check:
+#             return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+#         user = authenticate(request, username=username, password=password)
+        
+#         if user is not None:
+#             login(request, user)
+#             return Response({'message': "로그인 성공"}, status=status.HTTP_200_OK)
+#         else:
+#             return Response({'message': "로그인 실패"}, status=status.HTTP_400_BAD_REQUEST)
+
+
 class User_login(APIView):
 
-    queryset = MyUser.MyUser.objects.all()
-    serializer_class = UserSerializer.UserSerializer
-    
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        serializer = UserSerializer(data = request.data)
 
-        check, message = validate_input(username)
-        if not check:
-            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
-        
-        check, message = validate_password(username, password)
-        if not check:
-            return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            username = serializer.validated_data['username']
+            password = serializer.validated_data['password']
 
-        user = authenticate(request, username=username, password=password)
+            check, message = validate_input(username)
+
+            if not check:
+                return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
         
-        if user is not None:
-            login(request, user)
-            return Response({'message': "로그인 성공"}, status=status.HTTP_200_OK)
-        else:
-            return Response({'message': "로그인 실패"}, status=status.HTTP_400_BAD_REQUEST)
+            check, message = validate_password(username, password)
+            if not check:
+                return Response({'message': message}, status=status.HTTP_400_BAD_REQUEST)
+
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                login(request, user)
+                return Response({'message': "로그인 성공"}, status=status.HTTP_200_OK)
+            else:
+                return Response({'message': "로그인 실패"}, status=status.HTTP_400_BAD_REQUEST)
+
+
